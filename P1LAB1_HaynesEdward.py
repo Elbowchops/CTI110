cd181d4dda06f1ff08c511b399d13091e8cbeae3
Elbowchops <privilege224@gmail.com>
2025-02-05 12:03:02 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_HaynesEdward.py
@@ -0,0 +1,12 @@
+# Edward Haynes
+# 2/05/2025
+# P1LAB1
+# Using input and print functions
+
+# Get info from user
+
+f_name = input("Enter your first name:")
+l_name = input("Enter your last name:")
+
+# Display greeting to user
+print("Hello,", f_name, l_name + "!", "Welcome to CTI-110🙌🙌🙌")
